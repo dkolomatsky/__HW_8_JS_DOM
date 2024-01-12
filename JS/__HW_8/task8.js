3f12058dba000c639fe20e6d5d6f735381abff70
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-01-12 18:32:05 +0100

    виконані завдання 2, 5 , 8

--- JS/__HW_8/task8.js
+++ JS/__HW_8/task8.js
@@ -1,1 +1,12 @@
 // 8.Створює на сторінці 10 парахрафів і зробіть так, щоб при натисканні на параграф він зникав
+
+for (let i = 1; i < 11; i++) {
+  const parag = document.createElement("p");
+  document.body.append(parag);
+  parag.setAttribute("class", "parag");
+  parag.innerText = `Параграф № ${i}`;
+  parag.onclick = function () {
+    // parag.remove(); // видаляем параграф
+    parag.setAttribute("style", "color: transparent"); // робимо його невидимим
+  };
+}
