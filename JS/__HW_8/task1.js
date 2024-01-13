432074c5a8aa649a850a5590ad9db3b9b1be7f68
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-01-13 12:04:04 +0100

    создал необходимую разметку

--- JS/__HW_8/task1.js
+++ JS/__HW_8/task1.js
@@ -3,61 +3,78 @@
 // https://ru.megaindex.com/blog/files/images/semantic-markup-example.jpg
 
 // При подвійному кліку на тег має з'явитись input який прийматиме текст для тега по якому натиснули та новий фоновий колір
-const container = document.createElement("div");
+
+// const container = document.createElement("div");
 
 const headerTop = document.createElement("header");
-const headerSectionLeft = document.createElement("header");
-const headerSectionRight = document.createElement("header");
-const headerArticle1 = document.createElement("header");
-const headerArticle2 = document.createElement("header");
-
-const nav = document.createElement("nav");
-const navSectionRight = document.createElement("nav");
-
 const sectionLeft = document.createElement("section");
 const sectionRight = document.createElement("section");
-
-const article1 = document.createElement("article");
-const article2 = document.createElement("article");
-
-const asideArticle1 = document.createElement("aside");
-
-const pLongArticle1 = document.createElement("p");
-const pLongArticle2 = document.createElement("p");
-const p2longArticle2 = document.createElement("p");
-const pShortArticle1 = document.createElement("p");
-
 const footer = document.createElement("footer");
-const footerSectionLeft = document.createElement("footer");
-const footerAricle1 = document.createElement("footer");
-const footerArticle2 = document.createElement("footer");
+document.body.append(headerTop, sectionLeft, sectionRight, footer);
 
-document.body.innerText = "BODY";
-headerTop.innerText = "HEADER";
-nav.innerText = "NAV";
-headerSectionRight.innerText = "HEADER";
-navSectionRight.innerText = "NAV";
-sectionLeft.innerText = "SECTION";
-sectionRight.innerText = "SECTION";
-footer.innerText = "FOOTER";
-
-container.setAttribute("class", "container");
-headerTop.setAttribute("class", "header-top");
-headerSectionRight.setAttribute("class", "header-section__right");
-headerArticle1.setAttribute("class", "header-section__left--article1");
-headerArticle1.setAttribute("class", "header-section__left--article2");
-
-nav.setAttribute("class", "header-nav");
-navSectionRight.setAttribute("class", "section-nav__right");
-
-sectionLeft.setAttribute("class", "section-left");
-sectionRight.setAttribute("class", "section-right");
-
-footer.setAttribute("class", "footer");
-
-document.body.append(container);
-container.append(headerTop, sectionLeft, sectionRight, footer);
-
+const nav = document.createElement("nav");
 headerTop.append(nav);
 
-sectionRight.append(headerSectionRight, navSectionRight);
+const sectionLeftHeader = document.createElement("header");
+const article_1 = document.createElement("article");
+const article_2 = document.createElement("article");
+const sectionLeftFooter = document.createElement("footer");
+sectionLeft.append(sectionLeftHeader, article_1, article_2, sectionLeftFooter);
+
+const sectionRightHeader = document.createElement("header");
+const sectionRightNav = document.createElement("nav");
+sectionRight.append(sectionRightHeader, sectionRightNav);
+
+const article_1_Header = document.createElement("header");
+const article_1_Aside = document.createElement("aside");
+const aricle_1_Footer = document.createElement("footer");
+const article_1_ParagLong = document.createElement("p");
+const article_1_ParagShort = document.createElement("p");
+article_1.append(
+  article_1_Header,
+  article_1_ParagLong,
+  article_1_ParagShort,
+  article_1_Aside,
+  aricle_1_Footer
+);
+
+const article_2_Header = document.createElement("header");
+const article_2_parag1 = document.createElement("p");
+const article_2_parag2 = document.createElement("p");
+const article_2_Footer = document.createElement("footer");
+article_2.append(
+  article_2_Header,
+  article_2_parag1,
+  article_2_parag2,
+  article_2_Footer
+);
+
+// document.body.innerText = "BODY";
+// headerTop.innerText = "HEADER";
+// nav.innerText = "NAV";
+// headerSectionRight.innerText = "HEADER";
+// navSectionRight.innerText = "NAV";
+// sectionLeft.innerText = "SECTION";
+// sectionRight.innerText = "SECTION";
+// footer.innerText = "FOOTER";
+
+// container.setAttribute("class", "container");
+// headerTop.setAttribute("class", "header-top");
+// headerSectionRight.setAttribute("class", "header-section__right");
+// headerArticle1.setAttribute("class", "header-section__left--article1");
+// headerArticle1.setAttribute("class", "header-section__left--article2");
+
+// nav.setAttribute("class", "header-nav");
+// navSectionRight.setAttribute("class", "section-nav__right");
+
+// sectionLeft.setAttribute("class", "section-left");
+// sectionRight.setAttribute("class", "section-right");
+
+// footer.setAttribute("class", "footer");
+
+// document.body.append(container);
+// container.append(headerTop, sectionLeft, sectionRight, footer);
+
+// headerTop.append(nav);
+
+// sectionRight.append(headerSectionRight, navSectionRight);
