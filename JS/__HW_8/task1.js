204b0cec2eb526d17cdb35b49003936d3cac74ad
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-01-13 12:53:03 +0100

    создал имена каждому тегу

--- JS/__HW_8/task1.js
+++ JS/__HW_8/task1.js
@@ -4,32 +4,68 @@
 
 // При подвійному кліку на тег має з'явитись input який прийматиме текст для тега по якому натиснули та новий фоновий колір
 
-// const container = document.createElement("div");
-
+const body = document.querySelector("body");
+// основные блоки контейнеры
 const headerTop = document.createElement("header");
+// headerTop.textContent = "HEADER";
 const sectionLeft = document.createElement("section");
 const sectionRight = document.createElement("section");
 const footer = document.createElement("footer");
-document.body.append(headerTop, sectionLeft, sectionRight, footer);
 
-const nav = document.createElement("nav");
-headerTop.append(nav);
+// добавление в блок headerTop
+const navHeaderTop = document.createElement("nav");
 
+// добавление подблоков в блок sectionLeft
 const sectionLeftHeader = document.createElement("header");
 const article_1 = document.createElement("article");
 const article_2 = document.createElement("article");
 const sectionLeftFooter = document.createElement("footer");
-sectionLeft.append(sectionLeftHeader, article_1, article_2, sectionLeftFooter);
 
+// добавление подблоков в блок sectionRight
 const sectionRightHeader = document.createElement("header");
 const sectionRightNav = document.createElement("nav");
-sectionRight.append(sectionRightHeader, sectionRightNav);
 
+// добавление тегов в подблок article_1
 const article_1_Header = document.createElement("header");
 const article_1_Aside = document.createElement("aside");
 const aricle_1_Footer = document.createElement("footer");
 const article_1_ParagLong = document.createElement("p");
 const article_1_ParagShort = document.createElement("p");
+
+// добавление тегов в подблок article_2
+const article_2_Header = document.createElement("header");
+const article_2_parag1 = document.createElement("p");
+const article_2_parag2 = document.createElement("p");
+const article_2_Footer = document.createElement("footer");
+
+// присвоение имен тегов блоков,подблоков и конечных тегов в подблоках
+body.textContent = "BODY";
+headerTop.textContent = "HEADER";
+sectionLeft.textContent = "SECTION";
+sectionRight.textContent = "SECTION";
+footer.textContent = "FOOTER";
+navHeaderTop.textContent = "NAV";
+sectionLeftHeader.textContent = "HEADER";
+article_1.textContent = "ARTICLE";
+article_2.textContent = "ARTICLE";
+sectionLeftFooter.textContent = "FOOTER";
+sectionRightHeader.textContent = "HEADER";
+sectionRightNav.textContent = "NAV";
+article_1_Header.textContent = "HEADER";
+article_1_Aside.textContent = "ASIDE";
+aricle_1_Footer.textContent = "FOOTER";
+article_1_ParagLong.textContent = "P";
+article_1_ParagShort.textContent = "P";
+article_2_Header.textContent = "HEADER";
+article_2_parag1.textContent = "P";
+article_2_parag2.textContent = "P";
+article_2_Footer.textContent = "FOOTER";
+
+// добавление созданых элементов друг в друга
+body.append(headerTop, sectionLeft, sectionRight, footer);
+headerTop.append(navHeaderTop);
+sectionLeft.append(sectionLeftHeader, article_1, article_2, sectionLeftFooter);
+sectionRight.append(sectionRightHeader, sectionRightNav);
 article_1.append(
   article_1_Header,
   article_1_ParagLong,
@@ -37,28 +73,12 @@ article_1.append(
   article_1_Aside,
   aricle_1_Footer
 );
-
-const article_2_Header = document.createElement("header");
-const article_2_parag1 = document.createElement("p");
-const article_2_parag2 = document.createElement("p");
-const article_2_Footer = document.createElement("footer");
 article_2.append(
   article_2_Header,
   article_2_parag1,
   article_2_parag2,
   article_2_Footer
 );
-
-// document.body.innerText = "BODY";
-// headerTop.innerText = "HEADER";
-// nav.innerText = "NAV";
-// headerSectionRight.innerText = "HEADER";
-// navSectionRight.innerText = "NAV";
-// sectionLeft.innerText = "SECTION";
-// sectionRight.innerText = "SECTION";
-// footer.innerText = "FOOTER";
-
-// container.setAttribute("class", "container");
 // headerTop.setAttribute("class", "header-top");
 // headerSectionRight.setAttribute("class", "header-section__right");
 // headerArticle1.setAttribute("class", "header-section__left--article1");
@@ -71,10 +91,3 @@ article_2.append(
 // sectionRight.setAttribute("class", "section-right");
 
 // footer.setAttribute("class", "footer");
-
-// document.body.append(container);
-// container.append(headerTop, sectionLeft, sectionRight, footer);
-
-// headerTop.append(nav);
-
-// sectionRight.append(headerSectionRight, navSectionRight);
