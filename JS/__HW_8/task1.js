18b7d2c6eb6ad1416271db4f8302440b7ca51cf5
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-01-15 19:00:05 +0100

    зробив завдання 4

--- JS/__HW_8/task1.js
+++ JS/__HW_8/task1.js
@@ -4,81 +4,90 @@
 
 // При подвійному кліку на тег має з'явитись input який прийматиме текст для тега по якому натиснули та новий фоновий колір
 
-const body = document.querySelector("body");
-// основные блоки контейнеры
-const headerTop = document.createElement("header");
-// headerTop.textContent = "HEADER";
-const sectionLeft = document.createElement("section");
-const sectionRight = document.createElement("section");
-const footer = document.createElement("footer");
+const create = function () {
+  const body = document.querySelector("body");
+  // основные блоки контейнеры
+  const headerTop = document.createElement("header");
+  // headerTop.textContent = "HEADER";
+  const sectionLeft = document.createElement("section");
+  const sectionRight = document.createElement("section");
+  const footer = document.createElement("footer");
 
-// добавление в блок headerTop
-const navHeaderTop = document.createElement("nav");
+  // добавление в блок headerTop
+  const navHeaderTop = document.createElement("nav");
 
-// добавление подблоков в блок sectionLeft
-const sectionLeftHeader = document.createElement("header");
-const article_1 = document.createElement("article");
-const article_2 = document.createElement("article");
-const sectionLeftFooter = document.createElement("footer");
+  // добавление подблоков в блок sectionLeft
+  const sectionLeftHeader = document.createElement("header");
+  const article_1 = document.createElement("article");
+  const article_2 = document.createElement("article");
+  const sectionLeftFooter = document.createElement("footer");
 
-// добавление подблоков в блок sectionRight
-const sectionRightHeader = document.createElement("header");
-const sectionRightNav = document.createElement("nav");
+  // добавление подблоков в блок sectionRight
+  const sectionRightHeader = document.createElement("header");
+  const sectionRightNav = document.createElement("nav");
 
-// добавление тегов в подблок article_1
-const article_1_Header = document.createElement("header");
-const article_1_Aside = document.createElement("aside");
-const aricle_1_Footer = document.createElement("footer");
-const article_1_ParagLong = document.createElement("p");
-const article_1_ParagShort = document.createElement("p");
+  // добавление тегов в подблок article_1
+  const article_1_Header = document.createElement("header");
+  const article_1_Aside = document.createElement("aside");
+  const aricle_1_Footer = document.createElement("footer");
+  const article_1_ParagLong = document.createElement("p");
+  const article_1_ParagShort = document.createElement("p");
 
-// добавление тегов в подблок article_2
-const article_2_Header = document.createElement("header");
-const article_2_parag1 = document.createElement("p");
-const article_2_parag2 = document.createElement("p");
-const article_2_Footer = document.createElement("footer");
+  // добавление тегов в подблок article_2
+  const article_2_Header = document.createElement("header");
+  const article_2_parag1 = document.createElement("p");
+  const article_2_parag2 = document.createElement("p");
+  const article_2_Footer = document.createElement("footer");
 
-// присвоение имен тегов блоков,подблоков и конечных тегов в подблоках
-body.textContent = "BODY";
-headerTop.textContent = "HEADER";
-sectionLeft.textContent = "SECTION";
-sectionRight.textContent = "SECTION";
-footer.textContent = "FOOTER";
-navHeaderTop.textContent = "NAV";
-sectionLeftHeader.textContent = "HEADER";
-article_1.textContent = "ARTICLE";
-article_2.textContent = "ARTICLE";
-sectionLeftFooter.textContent = "FOOTER";
-sectionRightHeader.textContent = "HEADER";
-sectionRightNav.textContent = "NAV";
-article_1_Header.textContent = "HEADER";
-article_1_Aside.textContent = "ASIDE";
-aricle_1_Footer.textContent = "FOOTER";
-article_1_ParagLong.textContent = "P";
-article_1_ParagShort.textContent = "P";
-article_2_Header.textContent = "HEADER";
-article_2_parag1.textContent = "P";
-article_2_parag2.textContent = "P";
-article_2_Footer.textContent = "FOOTER";
+  // присвоение имен тегов блоков,подблоков и конечных тегов в подблоках
+  body.textContent = "BODY";
+  headerTop.textContent = "HEADER";
+  sectionLeft.textContent = "SECTION";
+  sectionRight.textContent = "SECTION";
+  footer.textContent = "FOOTER";
+  navHeaderTop.textContent = "NAV";
+  sectionLeftHeader.textContent = "HEADER";
+  article_1.textContent = "ARTICLE";
+  article_2.textContent = "ARTICLE";
+  sectionLeftFooter.textContent = "FOOTER";
+  sectionRightHeader.textContent = "HEADER";
+  sectionRightNav.textContent = "NAV";
+  article_1_Header.textContent = "HEADER";
+  article_1_Aside.textContent = "ASIDE";
+  aricle_1_Footer.textContent = "FOOTER";
+  article_1_ParagLong.textContent = "P";
+  article_1_ParagShort.textContent = "P";
+  article_2_Header.textContent = "HEADER";
+  article_2_parag1.textContent = "P";
+  article_2_parag2.textContent = "P";
+  article_2_Footer.textContent = "FOOTER";
 
-// добавление созданых элементов друг в друга
-body.append(headerTop, sectionLeft, sectionRight, footer);
-headerTop.append(navHeaderTop);
-sectionLeft.append(sectionLeftHeader, article_1, article_2, sectionLeftFooter);
-sectionRight.append(sectionRightHeader, sectionRightNav);
-article_1.append(
-  article_1_Header,
-  article_1_ParagLong,
-  article_1_ParagShort,
-  article_1_Aside,
-  aricle_1_Footer
-);
-article_2.append(
-  article_2_Header,
-  article_2_parag1,
-  article_2_parag2,
-  article_2_Footer
-);
+  // добавление созданых элементов друг в друга
+  body.append(headerTop, sectionLeft, sectionRight, footer);
+  headerTop.append(navHeaderTop);
+  sectionLeft.append(
+    sectionLeftHeader,
+    article_1,
+    article_2,
+    sectionLeftFooter
+  );
+  sectionRight.append(sectionRightHeader, sectionRightNav);
+  article_1.append(
+    article_1_Header,
+    article_1_ParagLong,
+    article_1_ParagShort,
+    article_1_Aside,
+    aricle_1_Footer
+  );
+  article_2.append(
+    article_2_Header,
+    article_2_parag1,
+    article_2_parag2,
+    article_2_Footer
+  );
+};
+create();
+
 // headerTop.setAttribute("class", "header-top");
 // headerSectionRight.setAttribute("class", "header-section__right");
 // headerArticle1.setAttribute("class", "header-section__left--article1");
--- JS/__HW_8/task4.js
+++ JS/__HW_8/task4.js
@@ -1,1 +1,24 @@
 // 4. Створіть 2 інпути та одну кнопку. Зробіть так, щоб інпути обмінювалися вмістом.
+const input1 = document.createElement("input");
+const input2 = document.createElement("input");
+const button = document.createElement("button");
+
+input1.setAttribute("id", "input1");
+input2.setAttribute("id", "input2");
+
+input1.setAttribute("type", "text");
+input1.setAttribute("name", "text");
+input1.setAttribute("value", "text from input1");
+input2.setAttribute("value", "text from input2");
+
+input2.setAttribute("type", "text");
+button.setAttribute("type", "button");
+button.textContent = "TRANSFER";
+
+button.onclick = function () {
+  const value = input1.value;
+  input1.value = input2.value;
+  input2.value = value;
+};
+
+document.body.append(input1, input2, button);
